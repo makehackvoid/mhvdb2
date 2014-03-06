11edc9ded3ae67839289e3ad2ccb5c4988005917
Cameron Moon <cameron@moon.net.au>
2014-03-06 22:42:29 +1100

    First draft of models

--- mhvdb2/models.py
+++ mhvdb2/models.py
@@ -1,6 +1,22 @@
 from mhvdb2 import app, database
 from peewee import *
 
+
 class BaseModel(Model):
     class Meta:
         database = database
+
+
+class Entity(Model):
+    name = CharField()
+    email = CharField()
+    is_member = BooleanField()
+
+
+class Payment(Model):
+    time = DateTimeField()
+    entity = ForeignKeyField(Entity, related_name='payments')
+    amount = IntegerField()
+    source = IntegerField()
+    is_donation = BooleanField()
+    notes = TextField()
