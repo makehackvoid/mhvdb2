81f75181efe8a52f612ca96b31f3d883eaaff83f
Cameron Moon <cameron@moon.net.au>
2014-03-07 14:38:41 +1100

    Added documentation to models, marked nullable fields as such

--- mhvdb2/models.py
+++ mhvdb2/models.py
@@ -8,15 +8,24 @@ class BaseModel(Model):
 
 
 class Entity(Model):
+    """
+    An Entity sends money to the organisation or recieves money from the
+    organistaion. Members are a special type of entity.
+    """
     name = CharField()
-    email = CharField()
-    is_member = BooleanField()
+    email = CharField(null=True)  # Email is required for members
+    is_member = BooleanField()  # Is the entity a member or past member
 
 
 class Payment(Model):
-    time = DateTimeField()
+    """
+    A Payment is a transaction between an entity and the organisation. A
+    payment can be either incoming or outgoing, depending on the sign of
+    "amount".
+    """
+    time = DateTimeField()  # Date & time the payment occured
     entity = ForeignKeyField(Entity, related_name='payments')
-    amount = IntegerField()
-    source = IntegerField()
-    is_donation = BooleanField()
-    notes = TextField()
+    amount = IntegerField()  # Monetary amount in cents, e.g. $12.34 = 1234
+    source = IntegerField(choice=[(0, 'Other'), (1, 'Bank Transfer')])
+    is_donation = BooleanField()  # For members, donation vs payment for goods
+    notes = TextField(null=True)
