63288a418e9c20384b49c220537b929a09b8be84
Cameron Moon <cameron@moon.net.au>
2014-04-21 15:59:17 +1000

    Added signup view and basic form

--- mhvdb2/views.py
+++ mhvdb2/views.py
@@ -5,3 +5,8 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup.html')
