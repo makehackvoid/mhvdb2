3c36c7e646b60b95c7c7d2ea1cbcbdfa24e42849
Kaley Halloran <kaleyhalloran@gmail.com>
2014-06-14 23:14:28 +1000

    Moved resources into own files

--- mhvdb2/resources/payment.py
+++ mhvdb2/resources/payment.py
@@ -5,44 +5,7 @@ import re
 from datetime import date, datetime
 from peewee import DoesNotExist
 
-class Resource:
-    pass
-
-class MemberResource(Resource):
-    @staticmethod
-    def validate(name, email, phone):
-        flashes = []
-        if not name:
-            flashes.append("Sorry, you need to provide a name.")
-        if not phone:
-            flashes.append("Sorry, you need to provide a phone number.")
-        if not re.match("[^@\s]+@[^@\s]+", email):
-            flashes.append("Sorry, you need to provide an email address.")
-
-        return flashes
-
-    @staticmethod
-    def create(name, email, phone):
-        member = Entity()
-        member.is_member = True
-        member.joined_date = date.today()
-        member.name = name
-        member.email = email
-        member.phone = phone
-        member.agreement_date = date.today()
-
-        return member.save()
-
-    @staticmethod
-    def update(member, name, email, phone):
-        member.name = name
-        member.email = email
-        member.phone = phone
-        member.agreement_date = date.today()
-
-        return member.save()
-
-class PaymentResource(Resource):
+class PaymentResource:
     @staticmethod
     def validate(amount, email, method, type, notes, reference):
         flashes = []
