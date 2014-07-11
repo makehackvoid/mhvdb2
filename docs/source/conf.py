8d70027c601186754de4f1ceecf30d2edfb50545
Brenda Moon <brenda@moon.net.au>
2014-07-11 18:44:05 +1000

    ignore sphinx config file for flake8 checking

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# flake8: noqa
 #
 # MHVdb2 documentation build configuration file, created by
 # sphinx-quickstart on Fri Jul 11 12:34:28 2014.
@@ -19,12 +20,12 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
